3e4a988b0ba4e8812138e7bc93b787131fc6a2cf
Amir Hamza <waliullahmunshi11@gmail.com>
2025-12-27 22:48:40 +0600

    env file add

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4dncfSjcKoXN3o8q6r92rPHNZ1JecAaU",
-  authDomain: "educational-project-6c227.firebaseapp.com",
-  projectId: "educational-project-6c227",
-  storageBucket: "educational-project-6c227.firebasestorage.app",
-  messagingSenderId: "630207394872",
-  appId: "1:630207394872:web:584d027962d9badb19e2cd",
-  measurementId: "G-L6MWWSR5V0"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
